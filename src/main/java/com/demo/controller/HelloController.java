53cb27cd2d80b389e594dea5c2e6e4d5ea4b6319
Administrator <751046424@qq.com>
2018-06-23 14:35:51 +0800

    hello

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.demo.controller;
 
 import com.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
